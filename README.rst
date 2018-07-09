492cc0eaa085e7d4100869bfb9a08d76d3d12160
Gilberto J Requena <Gilberto.Requena@zooplus.com>
2018-07-09 12:38:59 +0200

    Support queue url, error queue url and assume role
    * Remove error queue creation on listener initialization since it is created
      on launcher initialization
    * Add max num of messages param to listener

--- README.rst
+++ README.rst
@@ -48,11 +48,13 @@ Here is a basic code sample:
 | The options available as ``kwargs`` are as follows:
 
 - error_queue (str) - name of queue to push errors.
+- error_queue_url - url of the queue to push errors.
 - force_delete (boolean) - delete the message received from the queue, whether or not the handler function is successful.  By default the message is deleted only if the handler function returns with no exceptions
 - interval (int) - number of seconds in between polls. Set to 60 by default
 - visibility_timeout (str) - Number of seconds the message will be invisible ('in flight') after being read.  After this time interval it reappear in the queue if it wasn't deleted in the meantime.  Set to '600' (10 minutes) by default
 - error_visibility_timeout (str) - Same as previous argument, for the error queue.  Applicable only if the ``error_queue`` argument is set, and the queue doesn't already exist.
 - wait_time (int) - number of seconds to wait for a message to arrive (for long polling). Set to 0 by default to provide short polling.
+- max_number_of_messages (int) - Max number of messages to receive from the queue. Set to 1 by default, max is 10
 
 Running as a Daemon
 ~~~~~~~~~~~~~~~~~~~
@@ -142,6 +144,7 @@ Important Notes
    exist (in the specified region), it will be created at runtime.
 -  The error queue receives only two values in the message body: ``exception_type`` and ``error_message``. Both are of type ``str``
 -  If the function that the listener executes involves connecting to a database, you should explicitly close the connection at the end of the function.  Otherwise, you're likely to get an error like this: ``OperationalError(2006, 'MySQL server has gone away')``
+-  Either the queue name or the queue url should be provided. When both are provided the queue url is used and the queue name is ignored.
 
 Contributing
 ~~~~~~~~~~~~
--- sqs_launcher/__init__.py
+++ sqs_launcher/__init__.py
@@ -11,11 +11,12 @@ Created December 22nd, 2016
 # start imports
 # ================
 
+import json
+import logging
+import os
+
 import boto3
 import boto3.session
-import json
-import os
-import logging
 
 # ================
 # start class
@@ -23,11 +24,13 @@ import logging
 
 sqs_logger = logging.getLogger('sqs_listener')
 
+
 class SqsLauncher(object):
 
-    def __init__(self, queue, create_queue=False, visibility_timeout='600'):
+    def __init__(self, queue=None, queue_url=None, create_queue=False, visibility_timeout='600'):
         """
         :param queue: (str) name of queue to listen to
+        :param queue_url: (str) url of queue to listen to
         :param create_queue (boolean) determines whether to create the queue if it doesn't exist.  If False, an
                                     Exception will be raised if the queue doesn't already exist
         :param visibility_timeout: (str) Relevant to queue creation.  Indicates the number of seconds for which the SQS will hide the message.
@@ -35,32 +38,39 @@ class SqsLauncher(object):
                                     to finish execution. See http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-visibility-timeout.html
                                     for more information
         """
-        if not os.environ.get('AWS_ACCOUNT_ID', None):
-            raise EnvironmentError('Environment variable `AWS_ACCOUNT_ID` not set')
+        if not queue and not queue_url:
+            raise ValueError('Either `queue` or `queue_url` should be provided.')
+        if (not os.environ.get('AWS_ACCOUNT_ID', None) and
+                not (boto3.Session().get_credentials().method in ['iam-role', 'assume-role'])):
+            raise EnvironmentError('Environment variable `AWS_ACCOUNT_ID` not set and no role found.')
         # new session for each instantiation
         self._session = boto3.session.Session()
         self._client = self._session.client('sqs')
 
         self._queue_name = queue
-        queues = self._client.list_queues(QueueNamePrefix=self._queue_name)
-        exists = False
-        for q in queues['QueueUrls']:
-            qname = q.split('/')[-1]
-            if qname == self._queue_name:
-                exists = True
-                self._queue_url = q
+        self._queue_url = queue_url
+        if not queue_url:
+            queues = self._client.list_queues(QueueNamePrefix=self._queue_name)
+            exists = False
+            for q in queues['QueueUrls']:
+                qname = q.split('/')[-1]
+                if qname == self._queue_name:
+                    exists = True
+                    self._queue_url = q
 
-        if not exists:
-            if create_queue:
-                q = self._client.create_queue(
-                    QueueName=self._queue_name,
-                    Attributes={
-                        'VisibilityTimeout': visibility_timeout  # 10 minutes
-                    }
-                )
-                self._queue_url = q['QueueUrl']
-            else:
-                raise ValueError('No queue found with name ' + self._queue_name)
+            if not exists:
+                if create_queue:
+                    q = self._client.create_queue(
+                        QueueName=self._queue_name,
+                        Attributes={
+                            'VisibilityTimeout': visibility_timeout  # 10 minutes
+                        }
+                    )
+                    self._queue_url = q['QueueUrl']
+                else:
+                    raise ValueError('No queue found with name ' + self._queue_name)
+        else:
+            self._queue_name = self._get_queue_name_from_url(queue_url)
 
     def launch_message(self, message, **kwargs):
         """
@@ -77,8 +87,10 @@ class SqsLauncher(object):
                 MessageBody=json.dumps(message)
             )
         return self._client.send_message(
-                QueueUrl=self._queue_url,
-                MessageBody=json.dumps(message),
-                **kwargs
-            )
+            QueueUrl=self._queue_url,
+            MessageBody=json.dumps(message),
+            **kwargs
+        )
 
+    def _get_queue_name_from_url(self, url):
+        return url.split('/')[-1]
