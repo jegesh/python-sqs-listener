725441d30b39dbd97b341c93ee82fab50d7dbe2a
Adriean Khisbe <adriean.khisbe@live.fr>
2018-09-20 09:39:23 +0200

    Specify python3.6 support in setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
